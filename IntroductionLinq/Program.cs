62a494f3bafb9388dd0bb51f94655270c990dddc
Nutty4616 <nkosin361@gmail.com>
2022-09-24 20:15:03 +0200

    Initial commit

---
+++ IntroductionLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.IO;
+
+namespace IntroductionLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            foreach(var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+           
+    }
+   
+}
